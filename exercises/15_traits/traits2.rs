475fc53578c81774b61ba80c82b011a160ee7893
Hernán G. Gonzalez <camus.sonico@gmail.com>
2023-01-28 10:53:16 +0000

    traits

--- exercises/15_traits/traits2.rs
+++ exercises/15_traits/traits2.rs
@@ -8,13 +8,18 @@
 //
 // Execute `rustlings hint traits2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
 // TODO: Implement trait `AppendBar` for a vector of strings.
+impl AppendBar for Vec<String> {
+    fn append_bar(self) -> Self {
+        let mut me = self;
+        me.push("Bar".to_string());
+        me
+    }    
+}
 
 #[cfg(test)]
 mod tests {
